7da493a60b75ab6ac1577ac4802d2958b4bd492d
George Kando <gkando@gmail.com>
2019-07-31 10:59:56 -0500

    login page and authenication process complete. built basic template for user dashboard

--- conjugator/src/views/pages/Authentication/Authentication.js
+++ conjugator/src/views/pages/Authentication/Authentication.js
@@ -1,30 +1,19 @@
 import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Axios from 'axios';
 
-function MadeWithLove() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Built with love by the '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Material-UI
-      </Link>
-      {' team.'}
-    </Typography>
-  );
-}
+import { login } from '../../../state/actions';
+
 
 const useStyles = makeStyles(theme => ({
   '@global': {
@@ -51,25 +40,37 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-export default function SignUp() {
+function SignUp(props) {
   const classes = useStyles();
+
   const initialFormState = {
     id: null,
     firstName: '',
     lastName: '',
     email: '',
-    password: ''
+    password: '',
+    action: 'signup',
+    headerText: 'Sign Up Now!',
+    altAction: 'Already have an account? Sign in',
+    buttonText: 'Sign Up'
   };
+
   function handleSubmit(event) {
     event.preventDefault();
-    Axios.post('someUrl/signup',user)
-      .then(response => /*data*/
-        localStorage.setItem ('token', response.data.token)
-      )
-
-
-
-
+    const action = user.action;
+    const credentials = {email: user.email, password: user.password}
+    if (action == 'login'){
+      dispatch(login(credentials)).then(res => {
+        if (res) {
+          console.log(res)
+          props.history.push('/dashboard');
+        }
+      })
+    }
+    // Axios.post('someUrl/signup',user)
+    //   .then(response => /*data*/
+    //     localStorage.setItem ('token', response.data.token)
+    //   )
   }
   const [user, setUser] = useState(initialFormState);
 
@@ -78,6 +79,57 @@ export default function SignUp() {
     console.log(user);
     setUser(updatedUser);
   };
+  // const foo = useSelector(state => state.foo);
+
+  const dispatch = useDispatch()
+
+  const toggleForm = e => {
+    e.preventDefault();
+    if (user.action === 'login') { 
+      setUser(initialFormState)
+    } else {
+      const updatedForm = { 
+      ...user, 
+      headerText: 'Welcome Back! Sign in Below!',
+      altAction: 'Create An Account to Start Conjugating!',
+      action: 'login',
+      buttonText: 'Sign In'
+      };
+      setUser(updatedForm);
+    }
+  };
+
+  class FormSignUp extends React.Component {
+    render() {
+      return <>
+          <Grid item xs={12} sm={6}>
+            <TextField
+            autoComplete="fname"
+            name="firstName"
+            variant="outlined"
+            required
+            fullWidth
+            id="firstName"
+            label="First Name"
+            autoFocus
+            onChange={handleInputChange}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+            variant="outlined"
+            required
+            fullWidth
+            id="lastName"
+            label="Last Name"
+            name="lastName"
+            autoComplete="lname"
+            onChange={handleInputChange}
+            />
+          </Grid>
+        </>
+    }
+  }
   return (
     <Container component="main" maxWidth="xs">
       <CssBaseline />
@@ -85,36 +137,14 @@ export default function SignUp() {
         <Avatar className={classes.avatar}>
           <LockOutlinedIcon />
         </Avatar>
-        <Typography component="h1" variant="h5">
-          Sign up
+        <Typography component="h1" variant="h5" id='header-text'>
+          {/* Form Toggle Logic -- Same logic for the button and toggle link */}
+          { user.headerText }
         </Typography>
         <form className={classes.form} noValidate onSubmit={handleSubmit}>
           <Grid container spacing={2}>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                autoComplete="fname"
-                name="firstName"
-                variant="outlined"
-                required
-                fullWidth
-                id="firstName"
-                label="First Name"
-                autoFocus
-                onChange={handleInputChange}
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                id="lastName"
-                label="Last Name"
-                name="lastName"
-                autoComplete="lname"
-                onChange={handleInputChange}
-              />
-            </Grid>
+            {/* Render the 2 fields in FormSignUp if action = signup */}
+            { user.action === 'signup' ? <FormSignUp /> : null }
             <Grid item xs={12}>
               <TextField
                 variant="outlined"
@@ -140,12 +170,6 @@ export default function SignUp() {
                 onChange={handleInputChange}
               />
             </Grid>
-            <Grid item xs={12}>
-              <FormControlLabel
-                control={<Checkbox value="allowExtraEmails" color="primary" />}
-                label="I want to receive inspiration, marketing promotions and updates via email."
-              />
-            </Grid>
           </Grid>
           <Button
             type="submit"
@@ -154,20 +178,22 @@ export default function SignUp() {
             color="primary"
             className={classes.submit}
           >
-            Sign Up
+            {user.buttonText}
           </Button>
           <Grid container justify="flex-end">
             <Grid item>
-              <Link href="#" variant="body2">
-                Already have an account? Sign in
+              <Link 
+                href="#" 
+                variant="body2" 
+                onClick={toggleForm}
+              >
+                {user.altAction}
               </Link>
             </Grid>
           </Grid>
         </form>
       </div>
-      <Box mt={5}>
-        <MadeWithLove />
-      </Box>
     </Container>
   );
 }
+export default SignUp
