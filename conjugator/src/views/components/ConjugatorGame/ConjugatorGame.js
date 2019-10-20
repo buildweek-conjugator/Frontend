498bf022a0d50152ae860f9ec79b840bf6a97735
lilyhoratio <lilyhzhou@gmail.com>
2019-10-19 19:43:15 -0700

    comments

--- conjugator/src/views/components/ConjugatorGame/ConjugatorGame.js
+++ conjugator/src/views/components/ConjugatorGame/ConjugatorGame.js
@@ -13,7 +13,7 @@ import Chip from "@material-ui/core/Chip";
 
 import ProgressBar from "../ProgressBar/ProgressBar";
 
-import Loader from 'react-loader-spinner';
+import Loader from "react-loader-spinner";
 
 const data = {
   answer: "ubicaren",
@@ -25,41 +25,43 @@ const data = {
   has_long: false,
   translation: "to place; locate; to be located; be situated",
   value: ""
-}
+};
 
-const dataArr = [{
-  answer: "ubicaren",
-  performer: "ellos/ellas/ustedes",
-  mood: "Subjunctive",
-  infinitive: "ubicar",
-  performer_en: "them / you all (formal)",
-  tense: "Future",
-  has_long: false,
-  translation: "to place; locate; to be located; be situated",
-  value: ""
-},
-{
-  answer: "llamo",
-  performer: "yo",
-  mood: "Subjunctive",
-  infinitive: "llamar",
-  performer_en: "them / you all (formal)",
-  tense: "Present",
-  has_long: false,
-  translation: "to call",
-  value: ""
-},
-{
-  answer: "tañed",
-  performer: "ella",
-  mood: "Imperative Affirmative",
-  infinitive: "tañer",
-  performer_en: "them / you all",
-  tense: "Present",
-  has_long: false,
-  translation: "to play",
-  value: ""
-}];
+const dataArr = [
+  {
+    answer: "ubicaren",
+    performer: "ellos/ellas/ustedes",
+    mood: "Subjunctive",
+    infinitive: "ubicar",
+    performer_en: "them / you all (formal)",
+    tense: "Future",
+    has_long: false,
+    translation: "to place; locate; to be located; be situated",
+    value: ""
+  },
+  {
+    answer: "llamo",
+    performer: "yo",
+    mood: "Subjunctive",
+    infinitive: "llamar",
+    performer_en: "them / you all (formal)",
+    tense: "Present",
+    has_long: false,
+    translation: "to call",
+    value: ""
+  },
+  {
+    answer: "tañed",
+    performer: "ella",
+    mood: "Imperative Affirmative",
+    infinitive: "tañer",
+    performer_en: "them / you all",
+    tense: "Present",
+    has_long: false,
+    translation: "to play",
+    value: ""
+  }
+];
 
 // step1: axios.get - get relevant user settings for which verb forms to show
 // step2: axios.get - retrieve random unconjugated verb and tense and from data base
@@ -67,10 +69,8 @@ const dataArr = [{
 // step4: axios.post - posts request to check if answer is correct
 
 export default function ConjugatorGame() {
-
-
   const [verb, setVerb] = useState({});
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState("");
 
   const [completed, setCompleted] = useState(0);
   // const [verb, setVerb] = useState(dataArr[completed]);
@@ -79,65 +79,45 @@ export default function ConjugatorGame() {
   //random array of numbers to test
   const [usedWords, setUsedWords] = useState([1, 2, 5, 7, 9]);
 
-
-  function axiosGet () {
-
-    axios.get(`https://conjugator-bw-api.herokuapp.com/api/verbs/${getRandomInt(638)}`)
+  function axiosGet() {
+    axios
+      .get(
+        `https://conjugator-bw-api.herokuapp.com/api/verbs/${getRandomInt(638)}`
+      )
       .then(response => {
-        console.table("Question Data", response.data)
+        console.table("Question Data", response.data);
 
-        setVerb(response.data)
-        setInput('')
-        setAnswer(null)
-      })
+        setVerb(response.data);
+        setInput("");
+        setAnswer(null);
+      });
   }
 
-  useEffect( () => {
-    axiosGet()
- }, [] )
-
-
-
+  useEffect(() => {
+    axiosGet();
+  }, []);
 
   function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
-    // let randoInt = Math.floor(Math.random() * Math.floor(max));
-    // console.log(
-    //   randoInt,
-    //   " appears ",
-    //   usedWords.indexOf(randoInt),
-    //   " in the array"
-    // );
-    // //code to see if number has already been used this session
-    // //if it is not in the array (-1) its a good int
-    // //otherwise its a bad int and we need to get a new number
-    // if (usedWords.indexOf(randoInt) === -1) {
-    //   console.log("good int");
-    //   return randoInt;
-    // } else {
-    //   console.log("bad int");
   }
 
   const handleChange = event => {
     let textField = document.getElementById("outlined-name");
     textField.style = "background:white;";
-    setInput(event.target.value)
+    setInput(event.target.value);
   };
 
   const handleSubmit = event => {
     event.preventDefault();
-    // console.log("event.target.value", event.target.value)
     const targetInput = event.target.value;
-
 
     if (input.toLowerCase() === verb.answer) {
       setCompleted(completed + 1);
       targetInput.style = "background:#D9EFDE;";
 
       // placeholder to demo app until api is connected
-
       // setVerb(dataArr[completed+1])
-      axiosGet()
+      axiosGet();
       // setTimeout(function(){randomAxios()}, 2000)
     } else {
       targetInput.style = "background:#F8D7DA;";
@@ -147,6 +127,17 @@ export default function ConjugatorGame() {
       }, 2000);
     }
   };
+
+  /* Hey Airbnb!
+
+  I created a the front end to a Spanish conjugation game with a teammate who built the corresponding backend. 
+
+  I'm proud of writing this code as it was after two weeks of learning React and was my first React project I built
+  from scratch. I used React hooks to store state and made the front-end more dynamic based on user input. 
+  For example, in lines 141-152 and 206-217, I rendered a list of buttons with Spanish accented characters to 
+  the user, and altered a form input based on what accented character was selected. I initially hardcoded 
+  the accented character buttons and then later refactored it to make it DRY by mapping through chipData.
+  */
 
   const [chipData, setChipData] = useState([
     { key: 0, label: "á" },
@@ -158,18 +149,17 @@ export default function ConjugatorGame() {
   ]);
 
   const handleClick = chipClicked => () => {
-    setInput(input + `${chipClicked.label}`)
-
+    setInput(input + `${chipClicked.label}`);
   };
 
   const skipVerb = () => {
     // api call for new word
     // setVerb(dataArr[completed+1])
-    axiosGet()
+    axiosGet();
   };
 
   const toggleAnswer = () => {
-    console.log(answer)
+    console.log(answer);
     if (answer === null) {
       setAnswer(verb.answer);
     } else {
@@ -177,100 +167,103 @@ export default function ConjugatorGame() {
     }
   };
 
-  const [buttonText, setButtonText] = useState(false)
+  const [buttonText, setButtonText] = useState(false);
   const hoverOn = () => {
-    setButtonText(true)
+    setButtonText(true);
   };
 
   const hoverOff = () => {
-    setButtonText(false)
+    setButtonText(false);
   };
 
   return (
     <Paper className="game-container">
-  <>
-  {verb.infinitive === undefined ? (
-      <div className="key spinner">
-        <Loader
-            type="Puff"
-            color="#204963"
-            height="60"
-            width="60"
-        />
-        <p>Loading Data</p>
-      </div>
-    ) : (
+      <>
+        {verb.infinitive === undefined ? (
+          <div className="key spinner">
+            <Loader type="Puff" color="#204963" height="60" width="60" />
+            <p>Loading Data</p>
+          </div>
+        ) : (
+          <form
+            className="form-container"
+            noValidate
+            autoComplete="off"
+            onSubmit={handleSubmit}
+          >
+            <div className="tense-mood-container">
+              <span>{verb.tense_english} tense</span>
+              <span>{verb.mood_english} mood</span>
+            </div>
+            <div className="verb-translation-container">
+              <span className="verb">
+                {!verb.infinitive
+                  ? verb.infinitive
+                  : verb.infinitive.toUpperCase()}
+              </span>
+              <span className="translation">to {verb.short_english_verb}</span>
+            </div>
 
+            <div className="accent-characters">
+              {chipData.map(data => {
+                return (
+                  <Chip
+                    key={data.key}
+                    label={data.label}
+                    onClick={handleClick(data)}
+                  />
+                );
+              })}
+            </div>
 
-      <form
-        className="form-container"
-        noValidate
-        autoComplete="off"
-        onSubmit={handleSubmit}
-      >
+            <div className="subject-input-container">
+              <span id="subject">{verb.pronoun_spanish}</span>
 
-        <div className="tense-mood-container">
-          <span>{verb.tense_english} tense</span>
-          <span>{verb.mood_english} mood</span>
-        </div>
-        <div className="verb-translation-container">
-    <span className="verb">{!verb.infinitive ? verb.infinitive : verb.infinitive.toUpperCase()}</span>
-          <span className="translation">to {verb.short_english_verb}</span>
-        </div>
-
-        <div className="accent-characters">
-          {chipData.map(data => {
-            return (
-              <Chip
-                key={data.key}
-                label={data.label}
-                onClick={handleClick(data)}
+              <TextField
+                id="outlined-name"
+                name="value"
+                onChange={handleChange}
+                placeholder={
+                  answer ? `${verb.answer}` : `Enter your conjugation`
+                }
+                value={input}
+                margin="normal"
+                variant="outlined"
               />
-            );
-          })}
-        </div>
 
-        <div className="subject-input-container">
-          <span id="subject">{verb.pronoun_spanish}</span>
+              <Button
+                type="submit"
+                variant="contained"
+                color="primary"
+                className="submit-button"
+              >
+                Submit
+              </Button>
+            </div>
 
-          <TextField
-            id="outlined-name"
-            name="value"
-            onChange={handleChange}
-            placeholder={answer ? `${verb.answer}` : `Enter your conjugation`}
-            value={input}
-            margin="normal"
-            variant="outlined"
-          />
-
-          <Button
-            type="submit"
-            variant="contained"
-            color="primary"
-            className="submit-button"
-          >
-            Submit
-          </Button>
-        </div>
-
-        <Grid item>
-          <ButtonGroup
-            variant="contained"
-            size="small"
-            aria-label="small contained button group"
-          >
-            <Button onClick={skipVerb} onMouseEnter={hoverOn} onMouseLeave={hoverOff}>{buttonText ? " Pase " : "Skip"}</Button>
-            <Button onClick={toggleAnswer}>
-              {answer ? `Hide Answer` : `Show Answer`}
-            </Button>
-          </ButtonGroup>
-        </Grid>
-        {/* {answer && <p>{data.answer}</p>} */}
-        <ProgressBar className="progress-bar" completed={completed} />
-      </form>
-
-    )}
-    </>
+            <Grid item>
+              <ButtonGroup
+                variant="contained"
+                size="small"
+                aria-label="small contained button group"
+              >
+                <Button
+                  onClick={skipVerb}
+                  onMouseEnter={hoverOn}
+                  onMouseLeave={hoverOff}
+                >
+                  {buttonText ? " Pase " : "Skip"}
+                </Button>
+                <Button onClick={toggleAnswer}>
+                  {answer ? `Hide Answer` : `Show Answer`}
+                </Button>
+              </ButtonGroup>
+            </Grid>
+            {/* {answer && <p>{data.answer}</p>} */}
+            <ProgressBar className="progress-bar" completed={completed} />
+          </form>
+        )}
+      </>
     </Paper>
   );
 }
